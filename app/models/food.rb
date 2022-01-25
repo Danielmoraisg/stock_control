743bb3fa237c29c41924d5aa764bf6f8868b8cdd
Daniel Gusmão de Morais <danielgmoraisg@gmail.com>
2022-01-25 11:18:13 -0300

    Updated food.rb
    added length validation to the name param

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ApplicationRecord
-    validates :name, presence:true
+    validates :name, presence: true, length: {minimum: 2, maximum: 30}
+    validates :quantity, presence: true
+    validates :spoilage, presence: true
 end
